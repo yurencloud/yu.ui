0a7d91bd7223a3542caf7aa0e61083ca0ac78dab
jqz <jqz2cc@163.com>
2018-03-15 10:04:19 +0800

    'steps进行中'

---
+++ src/components/step.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'YuStep',
+}
+</script>
+
+<style scoped lang="scss" type="text/scss">
+  @import "../assets/css/varible";
+  @import "../assets/css/function";
+</style>
---
+++ src/components/steps.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'YuSteps',
+}
+</script>
+
+<style lang="scss" type="text/scss" scoped>
+  @import "../assets/css/varible";
+  @import "../assets/css/function";
+</style>
